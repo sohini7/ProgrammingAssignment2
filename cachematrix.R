7ca782cb66c0c4cc226c92a91ea95a1c8e33dbfb
sohini7 <sohiniand7@gmail.com>
2020-07-25 14:48:57 +0530

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,17 @@
 ##week 3 programming assignment 2
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
 ##This function creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
   j <- NULL
   set <- function(y){
     x <<- y
-    j <<- NULL
+    j <<- NULL                                           ##j will take value of matrix inverse
   }
   get <- function()x
-  setInverse <- function(inverse) j <<- inverse
+  setInverse <- function(inverse) j <<- inverse          ## Defining the set function to assign new value of matrix in parent environment
   getInverse <- function() j 
   list(set = set, get = get, 
        setInverse = setInverse, 
